7d62480383f9fc0eda99bf3f019789937d22e239
Ahmed Grwan <ahmed0grwan@gamil.com>
2020-01-04 10:34:19 +0200

    a complete mern app

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -1,26 +1,73 @@
 import React,{ useState, useEffect} from 'react';
 import queryString from 'query-string';
 import io from 'socket.io-client';
+import './Chat.css';
+import InfoBar from '../infoBar/InfoBar';
+import Input from '../input/Input';
+import Messages from '../messages/Messages';
+import TextContainer from '../textContainer/TextContainer';
+
+let socket;
 const Chat = props=>{
+    const [users, setUsers] = useState('');
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
+    const [messages, setMessages] = useState([]);
+    const [message, setMessage] = useState('');
     const ENDPOINT = 'localhost:5000';
     useEffect(()=>{
         const {name,room} = queryString.parse(props.location.search);
         setRoom(room);
         setName(name);
 
-        const socket = io(ENDPOINT);
+        socket = io(ENDPOINT);
         
-        socket.emit('join',{name,room});
+        socket.emit('join',{name,room},error=>{
+            if(error){
+                alert(error);
+            }
+        });
+
+        socket.on('roomData', ({ users }) => {
+            setUsers(users);
+          })
 
         return ()=>{
             socket.emit('disconnect');
             socket.off();
         }
     },[ENDPOINT,props.location.search]);
+
+    useEffect(()=>{
+        socket.on('message',message=>{
+            setMessages([...messages,message])
+        });
+    },[messages]);
+
+    const sendMessage = event=>{
+        event.preventDefault();
+
+        if(message){
+            socket.emit('sendMessage',message,()=>setMessage(''));
+        }
+    }
+
+    console.log(message,messages);
+
     return (
-        <h1>this is chat</h1>
+        <div className='outerContainer'>
+            <div className='container'>
+                <InfoBar room={room} />
+                <Messages messages={messages} name={name} />
+                {/* <input 
+                value={message}
+                onChange={event =>setMessage(event.target.value)}
+                onKeyPress={event=> event.key ==='Enter' ? sendMessage(event) : null}
+                /> */}
+                <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
+            </div>
+            <TextContainer users={users}/>
+        </div>
     );
 }
 
